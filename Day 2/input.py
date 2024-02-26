205a339b2c54fb4f5fdbe286131293d48fc983bd
Xelon6 <159533967+Xelon6@users.noreply.github.com>
2024-02-26 21:00:26 +0100

    Day 1 Print

---
+++ Day 2/input.py
@@ -0,0 +1,8 @@
+name = input("What is your name?: ")
+favorite_food = input("What is your favorite food?: ")
+favorite_music = input("What is your favorite music?: ")
+location = input("Where do you live?: ")
+
+print(
+    f"Hey there {name} i see that you like to eat {favorite_food} and listen to {favorite_music}? I also heard that it is nice at {location}, i bet you really enjoy living there!"
+)
